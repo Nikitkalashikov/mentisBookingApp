d1369bf7e6e8afb760633660d00a58dbb65e06b8
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-08-31 23:53:33 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import dotenv from "dotenv"
 
-// https://vitejs.dev/config/
 // export default defineConfig({
 // 	plugins: [react()],
 // })
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "")
+export default defineConfig(() => {
+	dotenv.config()
+
 	return {
 		plugins: [react()],
-		define: {
-			__VITE_DOCTORS_URL__: JSON.stringify(env.VITE_DOCTORS_URL),
-			__VITE_TOKEN_URL__: JSON.stringify(env.VITE_TOKEN_URL),
-			__VITE_USERNAME__: JSON.stringify(env.VITE_USERNAME),
-			__VITE_PASSWORD__: JSON.stringify(env.VITE_PASSWORD),
-		},
 	}
 })
