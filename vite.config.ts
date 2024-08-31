a4c567d6a025ffa74cca847988414979eb3e146b
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-08-31 23:06:11 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,4 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
 	plugins: [react()],
-	define: {
-		"process.env.VITE_DOCTORS_URL": JSON.stringify(
-			process.env.VITE_DOCTORS_URL || ""
-		),
-		"process.env.VITE_TOKEN_URL": JSON.stringify(
-			process.env.VITE_TOKEN_URL || ""
-		),
-		"process.env.VITE_USERNAME": JSON.stringify(
-			process.env.VITE_USERNAME || ""
-		),
-		"process.env.VITE_PASSWORD": JSON.stringify(
-			process.env.VITE_PASSWORD || ""
-		),
-	},
 })
