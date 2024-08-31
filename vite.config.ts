98f18cf8f420458191d414ce09c92a915d8ac046
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-08-31 22:56:59 +0300

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd())
-
-	return {
-		plugins: [react()],
-		define: {
-			__USERNAME__: JSON.stringify(env.VITE_USERNAME),
-		},
-	}
+export default defineConfig({
+	plugins: [react()],
+	define: {
+		"process.env.VITE_DOCTORS_URL": JSON.stringify(
+			process.env.VITE_DOCTORS_URL || ""
+		),
+		"process.env.VITE_TOKEN_URL": JSON.stringify(
+			process.env.VITE_TOKEN_URL || ""
+		),
+		"process.env.VITE_USERNAME": JSON.stringify(
+			process.env.VITE_USERNAME || ""
+		),
+		"process.env.VITE_PASSWORD": JSON.stringify(
+			process.env.VITE_PASSWORD || ""
+		),
+	},
 })
