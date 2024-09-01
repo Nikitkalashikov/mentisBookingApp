d76f589447d597566acfbadb81bd2b39f8bd24f3
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-09-02 00:02:53 +0300

    Update Card Doctor styles

--- src/components/Doctor/DoctorList/styled.tsx
+++ src/components/Doctor/DoctorList/styled.tsx
@@ -4,6 +4,7 @@ import { DoctorCard } from "../DoctorCard"
 export const DoctorListWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
+	align-items: center;
 `
 
 export const DoctorListCard = styled(DoctorCard)`
