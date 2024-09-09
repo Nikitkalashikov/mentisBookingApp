8c8f8fcad4d9bb0029413c9b62c99c22b92f4098
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-09-09 22:03:07 +0300

    Update telegram type

---
+++ src/services/telegram-web-app.d.ts
@@ -0,0 +1,16 @@
+interface Window {
+	Telegram: {
+		WebApp: {
+			initData: string
+			initDataUnsafe: {
+				user: {
+					first_name: string
+					last_name: string
+					is_bot: boolean
+					username: string
+					photo_url: string
+				}
+			}
+		}
+	}
+}
