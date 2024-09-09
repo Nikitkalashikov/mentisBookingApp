a54e6aec2400b306337ef9a4e7cd1103aea15565
Nikit Kalashnikov <kalashnikov.n.vl@gmail.com>
2024-09-09 21:57:37 +0300

    Add telegram api

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
+import { useTelegram } from "../../hooks/useTelegram"
 import { Container } from "../Container"
 import { Logo } from "../Logo"
 import { HeaderWrapper } from "./styled"
 
 function Header() {
+	const { user } = useTelegram()
+
 	return (
 		<HeaderWrapper>
 			<Container>
 				<Logo />
+				{user && <p>Привет {user.first_name}</p>}
 			</Container>
 		</HeaderWrapper>
 	)
